436acf32496443badb254b3b3a2ff2eff6c50a98
stephenheap <stephen.heap@softwire.com>
2016-11-09 17:13:17 +0000

    Added routing for the different languages

---
+++ RefugeeHousing/ActionFilterAttributes/LocalizationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace RefugeeHousing.ActionFilterAttributes
+{
+    public class LocalizationAttribute : ActionFilterAttribute 
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var language = (string) filterContext.RouteData.Values["language"] ?? "en";
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(language);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(language);
+        }
+    }
+}
---
+++ RefugeeHousing/Models/Language.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RefugeeHousing.Models
+{
+    public enum Language
+    {
+        English,
+        Greek 
+    }
+
+    public static class LanguageExtensions
+    {
+        public static string GetName(this Language language)
+        {
+            switch (language)
+            {
+                case Language.English:
+                    return "English";
+                case Language.Greek:
+                    return "ελληνικά";
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public static string GetCode(this Language language)
+        {
+            switch (language)
+            {
+                case Language.English:
+                    return "en";
+                case Language.Greek:
+                    return "el";
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
