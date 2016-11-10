5a3e089fb0d5f23ee861cd88f70419029fa3abc6
stephenheap <stephen.heap@softwire.com>
2016-11-10 12:14:58 +0000

    Added Translation Controller to contain the action for changing languages

---
+++ RefugeeHousing/Controllers/TranslationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using RefugeeHousing.Models;
+using RefugeeHousing.Translations;
+
+namespace RefugeeHousing.Controllers
+{
+    public class TranslationController : Controller
+    {
+        // GET: Translation
+        public ActionResult SetLanguage(Language language)
+        {
+            var service = new TranslationService();
+            service.SetTranslationCookie(language);
+            service.SetTranslationFromCookieIfExists();
+
+            var urlReferrer = Request.UrlReferrer;
+            if (urlReferrer != null)
+            {
+                return Redirect(urlReferrer.PathAndQuery);
+            }
+            return RedirectToAction("Index", "Home"); ;
+        }
+    }
+}
