c5f0212d9c34d00ed6249acd80c21ddc6b831d02
stephenheap <stephen.heap@softwire.com>
2016-11-10 17:57:30 +0000

    Listings are now pulled from database into a list view

---
+++ RefugeeHousing/Controllers/ListingsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using RefugeeHousing.Models;
+
+namespace RefugeeHousing.Controllers
+{
+    public class ListingsController : Controller
+    {
+        public ActionResult List()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var listingsList = (new ListingsList {Listings = db.Listings.ToList()});
+                return View(listingsList);
+            }
+        }
+    }
+}
---
+++ RefugeeHousing/Models/ListingsList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RefugeeHousing.Models
+{
+    public class ListingsList
+    {
+        public List<Listing> Listings { get; set; }
+    }
+}
