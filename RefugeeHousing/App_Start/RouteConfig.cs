8c6e91fda2d5515514118f04aec9bbd0744086bd
stephenheap <stephen.heap@softwire.com>
2016-11-14 11:30:06 +0000

    Added comment to explain routing change

--- RefugeeHousing/App_Start/RouteConfig.cs
+++ RefugeeHousing/App_Start/RouteConfig.cs
@@ -8,6 +8,11 @@ namespace RefugeeHousing
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //This changes the priority of where routing looks - by default directories are checked before controllers.
+            //Setting RouteExistingFiles to true tells ASP.net to always do the routing, even if a file/folder already exists.
+            //This was done because of the name clash between the PropertiesController and the Properties folder
+            //The name PropertiesController was desired to create /Properties in the url
             routes.RouteExistingFiles = true;
             routes.MapRoute(
                 name: "Default",
