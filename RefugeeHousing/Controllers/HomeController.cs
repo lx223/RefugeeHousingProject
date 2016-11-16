b65f9ebdf4af91fadcb6bce6942419002daa4e4b
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2016-11-16 17:01:56 +0000

    REF-32: Configure NLog logging

--- RefugeeHousing/Controllers/HomeController.cs
+++ RefugeeHousing/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Web.Mvc;
+using NLog;
 
 namespace RefugeeHousing.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public ActionResult Index()
         {
+            Logger.Info("Homepage requested");
+
             return View();
         }
     }
