29ceb3c8e95b68b1a2af937bad4801541067ccbb
Bob Chen <Bob.Chen@softwire.com>
2016-11-22 15:57:52 +0000

    Added AddListing view model

---
+++ RefugeeHousing/ViewModels/AddListing.cs
@@ -0,0 +1,13 @@
+﻿namespace RefugeeHousing.ViewModels
+{
+    public class AddListing
+    {
+        public string LanguagesSpoken { get; set; }
+        public string PlaceId { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public bool Furnished { get; set; }
+        public string Appliances { get; set; }
+        public bool Elevator { get; set; }
+    }
+}
