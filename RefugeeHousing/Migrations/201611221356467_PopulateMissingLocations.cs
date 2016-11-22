9db9b8abad90d907b130c832e874fc9cd5240694
Bob Chen <Bob.Chen@softwire.com>
2016-11-22 14:19:00 +0000

    Make sure that all the properties in the database have a location

---
+++ RefugeeHousing/Migrations/201611221356467_PopulateMissingLocations.cs
@@ -0,0 +1,23 @@
+﻿namespace RefugeeHousing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMissingLocations : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Locations (Id,EnglishName,GreekName) VALUES" +
+           "('ChIJ8UNwBh-9oRQR3Y1mdkU1Nic','Athens',N'Αθήνα')");
+
+            Sql("UPDATE Listings " +
+                "SET LocationId = (SELECT TOP 1 Id FROM Locations) " +
+                "WHERE LocationId IS NULL; ");
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
