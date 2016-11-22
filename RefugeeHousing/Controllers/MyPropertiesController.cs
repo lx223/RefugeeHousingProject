28a2c32820c16ab829972ce1dfcb4e2738c12a08
Bob Chen <Bob.Chen@softwire.com>
2016-11-22 16:45:56 +0000

    Added place_id hidden field and now it gets set when the location field changes

--- RefugeeHousing/Controllers/MyPropertiesController.cs
+++ RefugeeHousing/Controllers/MyPropertiesController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using RefugeeHousing.Models;
+using RefugeeHousing.ViewModels;
 
 namespace RefugeeHousing.Controllers
 {
@@ -12,20 +13,20 @@ namespace RefugeeHousing.Controllers
         [HttpGet]
         public ActionResult Add()
         {
-            var listing = new Listing();
-            return View(listing);
+            var addListing = new AddListing();
+            return View(addListing);
         }
 
         [HttpPost]
-        public ActionResult Add(Listing listing)
+        public ActionResult Add(AddListing addListing)
         {
             using (var db = new ApplicationDbContext())
             {
                 var currentUserId = User.Identity.GetUserId();
                 var currentUser = db.Users.Single(u => u.Id == currentUserId);
 
-                listing.Owner = currentUser;
-                db.Listings.Add(listing);
+//                listing.Owner = currentUser;
+//                db.Listings.Add(listing);
 
                 db.SaveChanges();
             }
