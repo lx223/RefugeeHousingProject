1689ea5299b493e35b7cd16e775c3b6a5f2c2900
Bob Chen <Bob.Chen@softwire.com>
2016-11-23 15:33:41 +0000

    Modifed autoCompleteLocation JavaScript file to limit search results to locations in Greece only

--- RefugeeHousing/Scripts/autoCompleteLocation.js
+++ RefugeeHousing/Scripts/autoCompleteLocation.js
@@ -1,6 +1,8 @@
 ﻿function autoCompleteLocation() {
-    var places = new google.maps.places.Autocomplete(document.getElementById('location'));
-    google.maps.event.addListener(places, 'place_changed', function () {
-        document.getElementById('place_id').value = places.getPlace().place_id;
+    var places = new google.maps.places.Autocomplete(document.getElementById("location"), {
+        componentRestrictions: {"country": "gr"}
+    });
+    google.maps.event.addListener(places, "place_changed", function () {
+        document.getElementById("place_id").value = places.getPlace().place_id;
     });
 }
