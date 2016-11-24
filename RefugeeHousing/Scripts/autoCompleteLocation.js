e92f4003df754bb4abe17e32f4d858f35cba3403
Bob Chen <Bob.Chen@softwire.com>
2016-11-24 11:05:23 +0000

    Added validation of location to make sure it contains a administrative_area_level_5 component

--- RefugeeHousing/Scripts/autoCompleteLocation.js
+++ RefugeeHousing/Scripts/autoCompleteLocation.js
@@ -13,7 +13,28 @@ function placeChanged() {
     var place = autocomplete.getPlace();
     if (!place.geometry) {
         window.alert("No details available for input: '" + place.name + "'");
+    } else if (!isPlaceSpecific(place)) {
+        window.alert("Please input a more specific address.");
     } else {
         document.getElementById("place_id").value = place.place_id;
     }
+}
+
+function isPlaceSpecific(place) {
+    var addressComponents = place["address_components"];
+    if (containsAdministrativeAreaLevel5Address(addressComponents)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function containsAdministrativeAreaLevel5Address(addressComponents) {
+    for (var i = 0; i < addressComponents.length; i++) {
+        var types = addressComponents[i]["types"];
+        if (types.indexOf("administrative_area_level_5") != -1) {
+            return true;
+        }
+    }
+    return false;
 }
---
+++ RefugeeHousing/Scripts/placeIdValidator.js
@@ -0,0 +1,9 @@
+﻿window.onload = function() {
+    $("#add-property-form").submit(function(event) {
+        var placeId = $("#place_id").val();
+        if (placeId == "") {
+            alert("Please use Location autocomplete to input a valid address");
+            event.preventDefault();
+        } 
+    });
+};
