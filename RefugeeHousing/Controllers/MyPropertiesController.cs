4c6ec84fcafee0edf9ba1cfc8ce242ba26a2ab25
Bob Chen <Bob.Chen@softwire.com>
2016-11-24 17:32:33 +0000

    Added PropertyListingService and extracted the adding listing logic from the controller to this class

--- RefugeeHousing/Controllers/MyPropertiesController.cs
+++ RefugeeHousing/Controllers/MyPropertiesController.cs
@@ -20,33 +20,12 @@ namespace RefugeeHousing.Controllers
         [HttpPost]
         public ActionResult Add(ListingViewModel listingViewModel)
         {
-            using (var db = new ApplicationDbContext())
-            {
-                var locationId = listingViewModel.PlaceId;
-                var location = LocationRepository.GetOrCreateLocation(db, locationId);
-                
-                var currentUserId = User.Identity.GetUserId();
-                var currentUser = UserIdentityService.GetUser(db, currentUserId);
-
-                var listing = new Listing
-                {
-                    Appliances = listingViewModel.Appliances,
-                    Elevator = listingViewModel.Elevator,
-                    Furnished = listingViewModel.Furnished,
-                    Price = listingViewModel.Price,
-                    LanguagesSpoken = listingViewModel.LanguagesSpoken,
-                    Location = location,
-                    LocationId = locationId,
-                    NumberOfBedrooms = listingViewModel.NumberOfBedrooms,
-                    Owner = currentUser,
-                    OwnerId = currentUserId
-                };
-
-                db.Listings.Add(listing);
-                db.SaveChanges();
-            }
+            var currentUserId = User.Identity.GetUserId();
+            PropertyListingService.AddListingToDatabase(listingViewModel, currentUserId);
 
             return Redirect("/");
         }
+
+
     }
 }
---
+++ RefugeeHousing/Services/PropertyListingService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using RefugeeHousing.Models;
+using RefugeeHousing.ViewModels;
+
+namespace RefugeeHousing.Services
+{
+    public class PropertyListingService
+    {
+        public static void AddListingToDatabase(ListingViewModel listingViewModel, string currentUserId)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var locationId = listingViewModel.PlaceId;
+                var location = LocationRepository.GetOrCreateLocation(db, locationId);
+                var currentUser = UserIdentityService.GetUser(db, currentUserId);
+
+                var listing = new Listing
+                {
+                    Appliances = listingViewModel.Appliances,
+                    Elevator = listingViewModel.Elevator,
+                    Furnished = listingViewModel.Furnished,
+                    Price = listingViewModel.Price,
+                    LanguagesSpoken = listingViewModel.LanguagesSpoken,
+                    Location = location,
+                    LocationId = locationId,
+                    NumberOfBedrooms = listingViewModel.NumberOfBedrooms,
+                    Owner = currentUser,
+                    OwnerId = currentUserId
+                };
+
+                db.Listings.Add(listing);
+                db.SaveChanges();
+            }
+        }
+    }
+}
