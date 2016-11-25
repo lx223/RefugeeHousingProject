9bdd441af16782b9e1f56e7a59fdb220456e5d65
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2016-11-25 17:04:12 +0000

    REF-49: Load JS files with require.js to modularize the JS better

--- RefugeeHousing/App_Start/BundleConfig.cs
+++ RefugeeHousing/App_Start/BundleConfig.cs
@@ -18,13 +18,6 @@ namespace RefugeeHousing
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
-                      "~/Scripts/listingsTableFiltering.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
--- RefugeeHousing/Scripts/listingsTableFiltering.js
+++ RefugeeHousing/Scripts/listingsTableFiltering.js
@@ -1,50 +1,55 @@
-﻿$.fn.dataTable.ext.search.push(
+﻿require(["jquery", "datatables"], function($) {
+  $.fn.dataTable.ext.search.push(
     // ReSharper disable once UnusedParameter
-    function (settings, data, dataIndex) {
-        
-        const minBedroomsFilterText = parseInt($('#filter-min-bedrooms-text').val(), 10);
-        const maxPriceFilterText = parseInt($('#filter-max-price-text').val(), 10);
-        const shouldBeFurnishedFilterText = $('#filter-furnished-text').val();
-        const locationFilterText = $('#filter-location-text').val().toLowerCase();
+    function(settings, data, dataIndex) {
 
-        const listingBedrooms = parseFloat(data[1]);
-        const listingIsFurnished = data[2] === "Yes" ? "True" : "False";
-        const listingLocation = data[3].toLowerCase();
-        const listingPrice = parseInt(data[4].substring(1));
+      const minBedroomsFilterText = parseInt($('#filter-min-bedrooms-text').val(), 10);
+      const maxPriceFilterText = parseInt($('#filter-max-price-text').val(), 10);
+      const shouldBeFurnishedFilterText = $('#filter-furnished-text').val();
+      const locationFilterText = $('#filter-location-text').val().toLowerCase();
 
-        
-        if (!isNaN(minBedroomsFilterText) && listingBedrooms < minBedroomsFilterText) {
-            return false;
+      const listingBedrooms = parseFloat(data[1]);
+      const listingIsFurnished = data[2] === "Yes" ? "True" : "False";
+      const listingLocation = data[3].toLowerCase();
+      const listingPrice = parseInt(data[4].substring(1));
+
+
+      if (!isNaN(minBedroomsFilterText) && listingBedrooms < minBedroomsFilterText) {
+        return false;
+      }
+
+      if (!isNaN(maxPriceFilterText) && listingPrice > maxPriceFilterText) {
+        return false;
+      }
+
+      if (shouldBeFurnishedFilterText !== null && shouldBeFurnishedFilterText !== "") {
+        if (shouldBeFurnishedFilterText !== listingIsFurnished) {
+          return false;
         }
+      }
 
-        if (!isNaN(maxPriceFilterText) && listingPrice > maxPriceFilterText) {
-            return false;
+      if (locationFilterText !== null && locationFilterText !== "") {
+        if (!listingLocation.includes(locationFilterText)) {
+          return false;
         }
+      }
 
-        if (shouldBeFurnishedFilterText !== null && shouldBeFurnishedFilterText !== "") {
-            if (shouldBeFurnishedFilterText !== listingIsFurnished) {
-                return false;
-            }
-        }
-
-        if (locationFilterText !== null && locationFilterText !== "") {
-            if (!listingLocation.includes(locationFilterText)) {
-                return false;
-            }
-        }
-
-        return true;
+      return true;
     }
-);
+  );
 
-$(document).ready(function () {
-    var table = $('#listings-table').DataTable();
+  var table = $('#listings-table').DataTable();
 
-    $('#filter-min-bedrooms-text, #filter-max-price-text, #filter-furnished-text').change(function () {
-        table.draw();
-    });
+  // Event listener to the two range filtering inputs to redraw on input
+  $('#filter-min-bedrooms-text, #filter-max-price-text, #filter-furnished-text').change(function() {
+    table.draw();
+  });
 
-    $('#filter-location-text').on('input', function() {
-        table.draw();
-    });
+  $('#filter-min-bedrooms-text, #filter-max-price-text, #filter-furnished-text').change(function () {
+    table.draw();
+  });
+
+  $('#filter-location-text').on('input', function() {
+    table.draw();
+  });
 });
