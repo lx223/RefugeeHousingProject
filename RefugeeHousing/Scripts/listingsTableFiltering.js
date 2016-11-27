bf949fd78d6580c87efbe2f91992c2050a2d48c4
Bob Chen <Bob.Chen@Softwire.com>
2016-11-27 14:14:46 +0000

    Added a textbox on Index page for location filtering. Modified listingsTableFiltering JavaScript to implement the filtering logic

--- RefugeeHousing/Scripts/listingsTableFiltering.js
+++ RefugeeHousing/Scripts/listingsTableFiltering.js
@@ -5,10 +5,12 @@
         const minBedroomsFilterText = parseInt($('#filter-min-bedrooms-text').val(), 10);
         const maxPriceFilterText = parseInt($('#filter-max-price-text').val(), 10);
         const shouldBeFurnishedFilterText = $('#filter-furnished-text').val();
+        const locationFilterText = $('#filter-location-text').val().toLowerCase();
 
         const listingBedrooms = parseFloat(data[1]);
         const listingIsFurnished = data[2] === "Yes" ? "True" : "False";
-        const listingPrice = parseInt(data[3].substring(1));
+        const listingLocation = data[3].toLowerCase();
+        const listingPrice = parseInt(data[4].substring(1));
 
         
         if (!isNaN(minBedroomsFilterText) && listingBedrooms < minBedroomsFilterText) {
@@ -25,6 +27,12 @@
             }
         }
 
+        if (locationFilterText !== null && locationFilterText !== "") {
+            if (!listingLocation.includes(locationFilterText)) {
+                return false;
+            }
+        }
+
         return true;
     }
 );
@@ -34,6 +42,10 @@ $(document).ready(function () {
 
     // Event listener to the two range filtering inputs to redraw on input
     $('#filter-min-bedrooms-text, #filter-max-price-text, #filter-furnished-text').change(function () {
+        table.draw();
+    });
+
+    $('#filter-location-text').on('input', function() {
         table.draw();
     });
 });
