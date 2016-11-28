ac65c7b294f1eba977226d871cbd7b2f4b9cdfb9
Bob Chen <Bob.Chen@Softwire.com>
2016-11-28 13:46:51 +0000

    Added translation for Edit and Delete links

--- RefugeeHousing/Resources/LocalizedText.Designer.cs
+++ RefugeeHousing/Resources/LocalizedText.Designer.cs
@@ -151,6 +151,24 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Delete.
+        /// </summary>
+        public static string Delete {
+            get {
+                return ResourceManager.GetString("Delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Edit.
+        /// </summary>
+        public static string Edit {
+            get {
+                return ResourceManager.GetString("Edit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Elevator.
         /// </summary>
         public static string Elevator {
