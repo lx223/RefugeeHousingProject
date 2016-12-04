e0844434b33c5cec31fb82c5c20f5dafe171429b
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2016-12-04 10:07:15 +0000

    Fix decimal->integer price migration
    The migration did not handle the column constraints properly

--- RefugeeHousing/Migrations/201611301344004_ChangePriceToInteger.cs
+++ RefugeeHousing/Migrations/201611301344004_ChangePriceToInteger.cs
@@ -7,12 +7,35 @@ namespace RefugeeHousing.Migrations
     {
         public override void Up()
         {
+            var constraintDrop = @"declare @table_name nvarchar(256);
+                declare @col_name nvarchar(256);
+                set @table_name = N'Listings';
+                set @col_name = N'Price';
+
+                declare @Command nvarchar(max) = ''
+
+                select @Command = @Command + 'ALTER TABLE Listings DROP CONSTRAINT ' + d.name
+                from sys.tables t
+                    join
+                    sys.default_constraints d
+                        on d.parent_object_id = t.object_id
+                    join
+                    sys.columns c
+                        on c.object_id = t.object_id
+                        and c.column_id = d.parent_column_id
+                where t.name = @table_name
+                and c.name = @col_name;
+
+                execute (@Command)";
+
+            Sql(constraintDrop);
             AlterColumn("dbo.Listings", "Price", c => c.Int(nullable: false));
         }
         
         public override void Down()
         {
             AlterColumn("dbo.Listings", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            Sql("ALTER TABLE Listings ADD  DEFAULT 0 FOR Price");
         }
     }
 }
